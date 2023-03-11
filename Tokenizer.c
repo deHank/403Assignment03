dfb88d04b93a09bf8ab0df09c1535c2b51d26ca8
davidf <ferrufinoda2@vcu.edu>
2023-03-10 23:02:22 -0500

    initial commit

---
+++ Tokenizer.c
@@ -0,0 +1,9 @@
+#include "Tokenizer.h"
+
+//*inf is already open
+//*I need to modify int numLex
+//*And add the lexs to struct lexics
+_Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf){
+
+    return 0;
+}
---
+++ Tokenizer.h
@@ -0,0 +1,2 @@
+#include "Givens.h"
+_Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf);
