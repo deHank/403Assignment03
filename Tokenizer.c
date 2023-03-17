691a9bf5d06a3c40063e8451bb06d9010fadb085
davidf <ferrufinoda2@vcu.edu>
2023-03-17 01:48:46 -0400

    modified:   Analyzer

--- Tokenizer.c
+++ Tokenizer.c
@@ -29,6 +29,10 @@ _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf){
           start = end;
 		  printf("end is %d", end);
 		  printf("word is %s\n", word);
+
+
+          struct lexics lexeme = lexemmeGenerator(word, numLex);
+          aLex[*numLex - 1] = lexeme;
           memset(word, 0, sizeof(word));
         }
         }
@@ -37,18 +41,62 @@ _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf){
     return 0;
 }
 
+struct lexics lexemmeGenerator(char *str, int *numLex) {
+    struct lexics lexemme;
+    strcpy(lexemme.lexeme, str);
+    enum token;
+    if (strcmp(str, "(") == 0) {
+        lexemme.token = 0;
+    }
+    else if (strcmp(str, ")") == 0) {
+        lexemme.token = 1;
+    }
+    else if (strcmp(str, "{") == 0) {
+        lexemme.token = 2;
+    }
+    else if (strcmp(str, "}") == 0) {
+        lexemme.token = 3;
+    }
+    else if (strcmp(str, "=") == 0) {
+        lexemme.token = 6;
+    }
+    else if (strcmp(str, ",") == 0) {
+        lexemme.token = 7;
+    }
+    else if (strcmp(str, ";") == 0) {
+        lexemme.token = 8;
+    }
+    else if (strcmp(str, "+") == 0 || strcmp(str, "*") == 0 || strcmp(str, "!") == 0) {
+        lexemme.token = 9;
+    }
+    else if (strcmp(str, "%") == 0) {
+        lexemme.token = BINOP;
+    }
+    else {
+        printf("%s : Did not match any case.\n", str);
+    }
+   return lexemme;
+}
+
+
 int findLongestCommonSubstring(char *str, int start){
   //printf("length of string is %d \n", strlen(str));
   int size = strlen(str);
   int i = start;
   int length = 1;
-  
+  char prev = '\0';
 
   for(i = start+1; i < strlen(str); i++){
     char* test = str[i];
+    char curr = str[i];
+    char test1[3] = {prev, curr, '\0'};
     _Bool isWord = validIdentifier(&test);
     _Bool isNum = validNumber(&str[i]);
-    if(isWord){
+    if(strcmp(test1, "==") == 0) {  // check for '=='
+      length++;
+      i++;  // skip the second '=' character
+    }
+    else if(isWord){
       printf("yes! TEST IS%s", &test);
         length++;
       }
--- Tokenizer.h
+++ Tokenizer.h
@@ -3,4 +3,6 @@
 
 _Bool tokenizer(struct lexics *aLex, int *numLex, FILE *inf);
 
+struct lexics lexemmeGenerator(char *str, int *numLex);
+
 int findLongestCommonSubstring(char *str, int start);
